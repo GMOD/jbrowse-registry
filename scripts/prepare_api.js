e2a470c0ac71ba4136f1a091b4d7c81636cf23a6
Eric Rasche <rasche.eric@gmail.com>
2016-07-19 15:25:46 +0000

    convert to JS yaml2json

---
+++ scripts/prepare_api.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+yaml = require('js-yaml');
+fs   = require('fs');
+
+var doc = yaml.safeLoad(fs.readFileSync('plugins.yaml', 'utf8'));
+console.log(doc);
--- scripts/prepare_api.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-import sys, json, yaml
-
-data = yaml.load(sys.stdin)
-json.dump(data, sys.stdout, indent=2)
