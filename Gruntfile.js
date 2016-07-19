aafaa9b73f522b4986c8967e8093416c59e17c14
Eric Rasche <rasche.eric@gmail.com>
2016-07-19 17:15:20 +0000

    All the options

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,6 +35,7 @@ module.exports = function (grunt)
         'gh-pages': {
             options: {
                 base: 'dist',
+                repo: 'git@github.com:erasche/pluginapp.git',
                 origin: 'git@github.com:erasche/pluginapp.git',
                 remoteUrl: 'git@github.com:erasche/pluginapp.git'
             },
