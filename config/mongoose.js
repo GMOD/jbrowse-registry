80b27ff8d873ed8b92abfda31313679568578856
Eric Rasche <rasche.eric@gmail.com>
2016-07-19 10:59:45 +0000

    Not used

--- config/mongoose.js
+++
@@ -1,9 +0,0 @@
-var config = require('./config'),
-    mongoose = require('mongoose');
-
-module.exports = function() {
-    var db  = mongoose.connect(config.db);
-    require('../app/models/plugin.server.model');
-    return db;
-};
-
