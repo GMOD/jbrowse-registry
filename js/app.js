6567baf7b1455bf7a63f291564228ebafcd171e8
Colin <colin.diesh@gmail.com>
2017-06-14 15:05:07 -0400

    Update deps, Add location bar html5 mode, Test pagination, Add eslint

--- js/app.js
+++ js/app.js
@@ -1,109 +1,40 @@
 var angular = require('angular');
-require('angular-utils-pagination');
+var angularui = require('angular-ui-bootstrap');
 var plugin_data = require('./plugins.json');
 var app_data = require('../package.json');
 var spdxLicenses = require('spdx-licenses');
 
-var app = angular.module('plugD', [
-    'angularUtils.directives.dirPagination'
-]);
+angular
+    .module('gmodPlugins', ['ui.bootstrap'])
+    .config(['$locationProvider', function($locationProvider) {
+        $locationProvider.html5Mode(true);
+    }])
+    .controller("PluginController",['$scope', '$location', function($scope, $location) {
 
-var ghString = "https://github.com/";
-var bbString = "https://bitbucket.org/";
+        $scope.page = 1;
+        $scope.plugins = plugin_data;
+        $scope.numPerPage = 10;
+        $scope.version = app_data.version;
+        $scope.url = app_data.repository.url
 
-app.controller("PluginController",['$scope', '$location', function($scope,$location){
-    $scope.currentPage = 1;
-    $scope.filter = {perPage: 5};
-
-    $scope.getCurrentFilter = function(){
-        var search = $location.search();
-        return {
-            gmod: search.gmod || "",
-            term: search.term || "",
-            page: $scope.currentPage || 1,
-            perPage: search.perPage || 5,
-            sortKey: search.sortKey || "name",
-            order: search.order || "+",
-        }
-    }
-
-    $scope.updateLocation = function(filter){
-        $location.search({
-            gmod: filter.gmod,
-            page: $scope.currentPage,
-            perPage: filter.perPage || 5,
-            term: filter.term,
-            sortKey: filter.sortKey,
-            order: filter.order,
+        $scope.plugins = plugin_data.map(function(elem){
+            elem.id = elem.name.replace(/ /g, '-').replace(/[^A-Za-z0-9_-]/g, '');
+            if(elem.location.indexOf("https://github.com/")>=0){
+                elem.abbrev = elem.location.substring("https://github.com/".length) ;
+                elem.type = "github";
+            }
+            if(elem.location.indexOf("https://bitbucket.org/")>=0){
+                elem.abbrev = elem.location.substring("https://bitbucket.org/".length) ;
+                elem.type = "bitbucket";
+            }
+            return elem;
         });
-    }
-
-    $scope.filter = $scope.getCurrentFilter();
-
-    $scope.plugins = plugin_data.map(function(elem){
-        elem.id = elem.name.replace(/ /g, '-').replace(/[^A-Za-z0-9_-]/g, '');
-        if(elem.location.indexOf(ghString)>=0){
-            elem.abbrev = elem.location.substring(ghString.length) ;
-            elem.type = "github";
-        }
-        if(elem.location.indexOf(bbString)>=0){
-            elem.abbrev = elem.location.substring(bbString.length) ;
-            elem.type = "bitbucket";
-        }
-        return elem;
-    });
-
-    //// pagination function on page change
-    $scope.pageChangeHandler = function(newPage){
-        $scope.currentPage = newPage;
-        $scope.updateLocation($scope.filter);
-    };
-
-    // Watch query string for sync to $scope.filter
-    $scope.$watch(function(){
-        return $location.search();
-    }, function(value){
-        // No need to updateLocation because that was the source of the
-        // event.
-        $scope.filter = value;
-    });
-
-    // Watch the $scope.filter for changes and sync to query string
-    $scope.$watch(function(scope){
-        return scope.filter;
-    }, function(newValue, oldValue){
-        $scope.updateLocation(newValue);
-    }, true);
-
-
-}]);
-
-
-app.directive("pluginList", function(){
-    return {
-        restrict:"E",
-        templateUrl:"partials/plugin-list.html",
-        controller: "PluginController"
-    }
-});
-
-app.directive("versionInfo", function(){
-    return {
-        restrict:"E",
-        template: '<a style="color:white" href="{{ url }}">Version {{ version }}</a>',
-        link: function(scope, el){
-            scope.version = app_data.version;
-            scope.url = app_data.repository.url;
-        }
-    }
-});
-
-app.filter("spdx_formatter", ["$sce", function($sce){
-    return function(input) {
-        if (input) {
-            var license = spdxLicenses.spdx(input);
-            return $sce.trustAs('html', license.name.replace(/^"/, '').replace(/"$/, '').replace(/""/g, '"'));
-        }
-        return;
-    };
-}]);
+    }]).filter("spdx_formatter", ["$sce", function($sce){
+        return function(input) {
+            if (input) {
+                var license = spdxLicenses.spdx(input);
+                return $sce.trustAs('html', license.name.replace(/^"/, '').replace(/"$/, '').replace(/""/g, '"'));
+            }
+            return;
+        };
+    }]);
